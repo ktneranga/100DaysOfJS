73b25fb83fa79d67cafae48b63c5d9aaa7ebe47f
Teran Neranga <49773078+ktneranga@users.noreply.github.com>
2023-06-21 08:19:16 +0530

    Update s3.js

--- DAY005-Excel File Reader/src/s3.js
+++ DAY005-Excel File Reader/src/s3.js
@@ -3,8 +3,8 @@ const { PutObjectCommand, S3Client } = require("@aws-sdk/client-s3");
 // varibales for s3 bucket
 const bucketName = "sygnamax-image-bucket";
 const bucketRegion = "us-east-1";
-const accessKey = "AKIA2GJ6LNE25TF2WXR4";
-const secretAccessKey = "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";
+const accessKey = "";
+const secretAccessKey = "";
 
 const client = new S3Client({
   credentials: {
